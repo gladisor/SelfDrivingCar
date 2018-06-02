a4fffa6c448ef48f73a85cea9adbcf008a3d3066
Tristan <123.tristan.shah@gmail.com>
2018-06-01 18:22:17 -0700

    this is the first iteration of source code for self driving car

---
+++ src/autocar.cpp
@@ -0,0 +1,91 @@
+int LeftMotor = 12;
+int RightMotor = 13;
+int ECPin = 1;
+int TrigPin = 2;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LeftMotor, INPUT);
+  pinMode(RightMotor, INPUT);
+  pinMode(ECPin, INPUT);
+  pinMode(TrigPin, OUTPUT);
+}
+
+void loop()
+{
+  distance();
+  if (distance > 30)
+  {
+    DriveStraight();
+  }
+  else
+  {
+    Stop();
+    TurnRight();
+    if (distance > 30)
+    {
+      DriveStraight();
+    }
+    else
+    {
+      TurnLeft();
+      TurnLeft();
+      if (distance > 30)
+      {
+        DriveStraight();
+      }
+      else
+      {
+        Stop();
+      }
+    }
+  }
+}
+/*void loop() 
+{
+  DriveStraight()
+    delayMicroseconds(500);
+  Stop()
+    delayMicroseconds(500);
+}*/
+void DriveStraight()
+{
+  digitalWrite(LeftMotor, HIGH);
+  digitalWrite(RightMotor, HIGH);
+  delayMicroseconds(500);
+  Stop();
+}
+
+void TurnRight()
+{
+  digitalWrite(LeftMotor, HIGH);
+  digitalWrite(RightMotor, LOW);
+  delayMicroseconds(500);
+  Stop();
+}
+
+void TurnLeft()
+{
+  digitalWrite(LeftMotor, LOW);
+  digitalWrite(RightMotor, HIGH);
+}
+
+void Stop()
+{
+  digitalWrite(LeftMotor, LOW);
+  digitalWrite(RightMotor, LOW);
+}
+
+long distance()
+{
+  long duration, distance;
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+  duration = pulseIn(ECPin, LOW);
+  distance = (duration/2)/29.1;
+  return distance;
+}
